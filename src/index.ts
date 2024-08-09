170f4e87478e4345dfb4c9fb3ef3d0505147593b
T0ls <giugiust2002@gmail.com>
2024-08-09 05:17:36 +0200

    Samu698: added code for parsing the data

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,53 @@
-console.log("Hello, World")
+interface Poop {
+	name: string;
+	date: Date;
+}
+
+window.onload = () => {
+	const input: HTMLInputElement = document.querySelector("#fileInput");
+	console.log(input);
+	input.onchange = _ => {
+		const file = input.files[0];
+		parseFile(file);
+	}
+	input.click();
+}
+
+function parseFile(file: File): void {
+	const reader = new FileReader();
+	reader.readAsText(file, 'UTF-8');
+	reader.onload = event => {
+		const result: Poop[] = [];
+
+		const contentBuf = event.target.result;
+		const content = contentBuf.toString();
+		const lines = content.split(/\r?\n|\r|\n/g);
+		for (const line of lines) {
+			console.log(line);
+			if (!line.endsWith("💩")) continue;
+			const [ dateStr, rest ] = line.split(" - ", 2);
+			const endIndex = rest.indexOf(":");
+			const name = rest.substring(0, endIndex);
+			const date = parseDate(dateStr);
+
+			result.push({ name, date });
+		}
+
+		initializeGraphs(result);
+	}
+}
+
+function parseDate(date: string): Date {
+	const regex = /^(\d{1,2})\/(\d{1,2})\/(\d{1,2}), (\d{1,2}):(\d{1,2})$/;
+	const result = regex.exec(date);
+	if (result === null) return new Date(0);
+	const parts = result.slice(1).map(v => parseInt(v));
+	let [day, month, year, hour, minute] = parts;
+	if (year >= 70) { year += 1900; }
+	else { year += 2000; }
+	return new Date(year, month - 1, day, hour, minute)
+}
+
+function initializeGraphs(data: Poop[]): void {
+	console.log(data);
+}
